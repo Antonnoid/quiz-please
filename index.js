62e98c93dd012e25318a127cfc41dc569a529e4b
50chin <50chin50@gmail.com>
2023-06-09 15:04:09 +0300

    Kolbasa

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const readlineSync = require('readline-sync');
 
+const pathJoin = path.join(__dirname, './topics/Спорт.txt');
+
 const userName = readlineSync.question('Как тебя звать? ');
-console.log('Привет ' + userName + '!');
+console.log(`Привет ${userName}!`);
 
-
-
-// const favFood = readlineSync.question('What is your favorite food? ');
-// console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+async function myQuiz() {
+  const readFile = await fs.readFileSync(pathJoin, 'utf-8');
+  console.log(readFile);
+  for (let i = 0; i < readFile.length; i++) {}
+}
+const game = readlineSync.question('Желаешь сыграть в игру? ');
+if (game.toLowerCase() === 'да') {
+  console.log(`Спасибо за игру ${userName}!`, myQuiz());
+} else console.log(`Ну и пошел ты ${userName}!`);
