90662c02ba8847fca788db1c3a66a7a32e2c4a8c
50chin <50chin50@gmail.com>
2023-06-09 12:04:40 +0300

    Создал квиз про спорт

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-123
+const readlineSync = require('readline-sync');
+
+const userName = readlineSync.question('Как тебя звать? ');
+console.log('Привет ' + userName + '!');
+
+
+
+// const favFood = readlineSync.question('What is your favorite food? ');
+// console.log('Oh, ' + userName + ' loves ' + favFood + '!');
